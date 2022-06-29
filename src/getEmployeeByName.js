ddd081f42514d1691f90d607ee99225db1ce37ee
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-29 17:21:04 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return employees.find(({ firstName, lastName }) =>
+    firstName === employeeName || lastName === employeeName);
 }
+
+console.log(getEmployeeByName('Wishart'));
 
 module.exports = getEmployeeByName;
