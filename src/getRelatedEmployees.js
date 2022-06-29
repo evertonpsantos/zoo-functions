293c2043644d08a0de3b2e8697c8797abfb62740
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-29 19:10:19 -0300

    Reuisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  let managerTruthiness = false;
+  employees.forEach(({ managers }) => {
+    if (managers.some((element) => element === id)) managerTruthiness = true;
+  });
+  return managerTruthiness;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return employees.filter((employee) => (employee.managers.includes(managerId)))
+    .map((person) => `${person.firstName} ${person.lastName}`);
 }
+
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
