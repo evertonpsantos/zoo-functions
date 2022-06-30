07e7d9986472c9ec545b79d18c1916a0b9d0e861
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-30 17:12:31 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+const { adult, senior, child } = prices;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((acc, curr) => {
+    if (curr.age < 18) acc.child += 1;
+    if (curr.age >= 18 && curr.age < 50) acc.adult += 1;
+    if (curr.age >= 50) acc.senior += 1;
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculate(param) {
+  const counted = countEntrants(param);
+  let sum = 0;
+  if (Object.keys(counted).includes('child')) sum += (child * counted.child);
+  if (Object.keys(counted).includes('adult')) sum += (adult * counted.adult);
+  if (Object.keys(counted).includes('senior')) sum += (senior * counted.senior);
+  return sum;
+}
+
+function calculateEntry(param) {
+  if (!param || Object.keys(param).length === 0) return 0;
+  return calculate(param);
 }
 
 module.exports = { calculateEntry, countEntrants };
