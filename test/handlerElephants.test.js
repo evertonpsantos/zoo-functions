dae3a52373067cfb311b39c3b942ca55454a78b3
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-29 21:52:41 -0300

    Requisitos 6 e 7

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,26 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Testa se ao chamar a função sem parametro retorna undefined', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+  it('Testa se ao passar um número como parâmetro retorna "Parâmetro inválido, é necessário uma string"', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Testa se ao passar `count` como parâmetro retorna 4', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+  it('Testa se ao passar `averageAge` como parâmetro retorna 10.5', () => {
+    expect(handlerElephants('averageAge')).toBe(10.5);
+  });
+  it('Testa se ao passar `names` como parâmetro retorna ["Ilana", "Orval", "Bea", "Jefferson"]', () => {
+    const expected = ['Ilana', 'Orval', 'Bea', 'Jefferson'];
+    expect(handlerElephants('names')).toEqual(expect.arrayContaining(expected));
+  });
+  it('Testa se ao passar `batata` como parâmetro retorna null', () => {
+    expect(handlerElephants('batata')).toBe(null);
+  });
+  it('Testa se ao passar `batata` como parâmetro retorna null', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+});
