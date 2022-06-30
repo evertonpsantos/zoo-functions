c5758370abdbf2fa3053f05bb1fbc089f950363d
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-06-30 20:05:42 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstUnder = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const found = species.find((element) => element.id === firstUnder).residents
+    .reduce((acc, curr) => {
+      if (curr.age > acc.age) return curr;
+      return acc;
+    });
+  return Object.values(found);
 }
 
 module.exports = getOldestFromFirstSpecies;
